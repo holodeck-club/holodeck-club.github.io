79a5cc9df7c9e381c34e491c2765450c2c32516b
Christopher Van <cvan@mozilla.com>
2016-06-09 23:51:44 -0700

    add molo project for Mozilla London showcase

---
+++ molo/assets/js/voice-controls.js
@@ -0,0 +1,34 @@
+/* global WEBVR_VOICE_NAV */
+(function () {
+  if (!('WEBVR_VOICE_NAV' in window)) {
+    return;
+  }
+  WEBVR_VOICE_NAV.addItems([
+    {
+      title: 'A-Painter',
+      slug: 'apainter',
+      url: '/mozillavr/apainter/',
+      keywords: ['paint', 'painting', 'tiltbrush', 'tilt', 'brush'],
+    },
+    {
+      title: 'Droids Hunter',
+      slug: 'droidshunter',
+      url: '/mozillavr/droidshunter/'
+    },
+    {
+      title: 'Dynamic Lights',
+      slug: 'dynamic-lights',
+      url: '/mozillavr/dynamic-lights/'
+    },
+    {
+      title: 'Spheres and Fog',
+      slug: 'spheres-and-fog',
+      url: '/mozillavr/spheres-and-fog/'
+    },
+    {
+      title: 'Teleport',
+      slug: 'teleport',
+      url: '/mozillavr/teleport/'
+    }
+  ]);
+})();
